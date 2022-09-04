aad8e57bbe9b038ed6126b7c70de7663ca21c910
rony9843 <rony13647@gmail.com>
2022-09-04 04:21:50 +0300

    Initial commit

--- src/globeVar .js
+++ src/globeVar .js
@@ -1,2 +1,3 @@
-const globeSocketIo = "http://localhost:8800";
+//const globeSocketIo = "http://localhost:8800";
+const globeSocketIo = "https://queenzzone-socket-io.herokuapp.com/";
 export default globeSocketIo;
